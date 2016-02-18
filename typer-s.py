e8916696ab6109d638f676cdba000ac0ca5ed0cd
hhsecond <sherinct@live.com>
2016-02-18 12:37:21 +0530

    added exception handling and argument parsing

--- typer-s.py
+++ typer-s.py
@@ -1,5 +1,7 @@
-import pythoncom, pyHook, datetime, threading
-from core import objthread
+import pythoncom, pyHook, datetime, threading, time, sys
+from core import objthread_down, objthread_up
+print('initializing the thread.....')
+time.sleep(.5)
 
 def OnKeyboardEventD(event):
     with threading.Lock():
@@ -13,15 +15,28 @@ def OnKeyboardEventU(event):
 
 
 def main():
-    # create a hook manager
-    hm = pyHook.HookManager()
-    # watch for all mouse events
-    hm.KeyDown = OnKeyboardEventD
-    hm.KeyUp = OnKeyboardEventU
-    # set the hook
-    hm.HookKeyboard()
-    # wait forever
-    pythoncom.PumpMessages()
+    try:
+        if sys.argv[1] == '-l':
+            print('script started in listening mode')
+        if sys.argv[1] == '-e':
+            print('make sure that you have enough data in the tree to compare')
+            time.sleep(1)
+            print('script started in execution mode...')
+    except:
+        print('exception reaised: start the program in listening mode (-l) or execution mode (-e)')
+    else:
+        # create a hook manager
+        hm = pyHook.HookManager()
+        # watch for all mouse events
+        hm.KeyDown = OnKeyboardEventD
+        hm.KeyUp = OnKeyboardEventU
+        # set the hook
+        hm.HookKeyboard()
+        # wait forever
+        pythoncom.PumpMessages()
+
+
+
 
 
 '''import pythoncom, pyHook, datetime, threading
