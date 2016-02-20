d177a58050a55c4c8f3610b96df4438535d6f588
hhsecond <sherinct@live.com>
2016-02-21 02:40:07 +0530

    new argument parsing

--- typer-s.py
+++ typer-s.py
@@ -1,5 +1,5 @@
 import pythoncom, pyHook, datetime, threading, time, sys
-from core import objthread_down, objthread_up
+from core import objthread_down, objthread_up, dict_from_file
 print('initializing the thread.....')
 time.sleep(.5)
 
@@ -19,9 +19,10 @@ def main():
         if sys.argv[1] == '-l':
             print('script started in listening mode')
         if sys.argv[1] == '-e':
-            print('make sure that you have enough data in the tree to compare')
-            time.sleep(1)
+            print('Rading data from the tree, make sure you have enough')
+            dict_from_file()
             print('script started in execution mode...')
+
     except:
         print('exception reaised: start the program in listening mode (-l) or execution mode (-e)')
     else:
